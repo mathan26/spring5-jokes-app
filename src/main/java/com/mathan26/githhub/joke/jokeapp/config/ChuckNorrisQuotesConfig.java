252ee53c61be0218bd52427edeab48e2f3f66b33
mathan26 <mathan.se@gmail.com>
2020-02-29 08:39:46 +0530

    XML bean creation

--- src/main/java/com/mathan26/githhub/joke/jokeapp/config/ChuckNorrisQuotesConfig.java
+++ src/main/java/com/mathan26/githhub/joke/jokeapp/config/ChuckNorrisQuotesConfig.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class ChuckNorrisQuotesConfig {
-    @Bean
+   // @Bean
     public ChuckNorrisQuotes chuckNorrisQuotes(){
         return  new ChuckNorrisQuotes();
     }
